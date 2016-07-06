21ea34ee446a330e503b85b48d9a1843a249601c
Marcin Kiszewski <marcin.k@ragnak.com>
2016-07-06 14:52:00 +0200

    backend url
    Change-Id: I460a9defa1a4292ad5224d5ed47d6387c78565fa

--- js/hosts.js
+++ js/hosts.js
@@ -1,4 +1,4 @@
-// var BACKEND_HOST = 'http://85.219.144.28:8201';
-var BACKEND_HOST = 'http://localhost:3000';
-// var BACKEND_HOST = 'http://46.17.215.204:50001';
+// var BACKEND_HOST = 'http://localhost:3000';
+var BACKEND_HOST = 'http://46.17.215.204:50001';
+// var BACKEND_HOST = 'http://46.17.215.204:47176';
 // var MOBILE_SERVER_HOST = 'http://127.0.0.1:8001';
