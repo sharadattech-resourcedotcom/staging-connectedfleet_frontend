8451f189854fe4c8bee12fd109afa437375c7145
newbury <newbury@debian>
2016-12-20 12:36:17 +0000

    change server port

--- js/hosts.js
+++ js/hosts.js
@@ -1,4 +1,4 @@
 // var BACKEND_HOST = 'http://localhost:3000';
-var BACKEND_HOST = 'http://46.17.215.204:50001';
-// var BACKEND_HOST = 'http://46.17.215.204:47176';
+//var BACKEND_HOST = 'http://46.17.215.204:50001';
+var BACKEND_HOST = 'http://213.129.91.199:47176';
 // var MOBILE_SERVER_HOST = 'http://127.0.0.1:8001';
